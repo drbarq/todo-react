ea7136f2050759982b39833b207164d5fa4d23cf
drbarq <joe.tustin@datadoghq.com>
2022-02-14 16:20:47 -0700

    adding in datadog browser react rum

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { datadogRum } from '@datadog/browser-rum';
+    
+datadogRum.init({
+    applicationId: '73bf5c7e-7a50-449a-8305-e7fca9c296bb',
+    clientToken: 'pubc1cf342413899f2174fdfc2c998e57ac',
+    site: 'datadoghq.com',
+    service:'todo-app---react',
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel: 'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
+
 
 const DATA = [
   { id: "todo-0", name: "Eat", completed: true },
